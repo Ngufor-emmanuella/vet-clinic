0acb2deac322857b1af5e658b1c4bc1eb2080f29
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-01-06 11:49:23 +0100

    Add relations to table

--- queries.sql
+++ queries.sql
@@ -49,3 +49,20 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+-- project for multiple table relation
+SELECT a.name FROM animals a JOIN owners o ON a.owner_id = o.id WHERE o.full_name = 'Melody Pond';
+
+SELECT a.name FROM animals a JOIN species s ON a.species_id = s.id WHERE s.name = 'Pokemon';
+
+SELECT o.full_name, a.name FROM owners o LEFT JOIN animals a ON o.id = a.owner_id;
+
+SELECT count(*), s.name FROM animals a JOIN species s ON a.species_id = s.id GROUP BY s.name;
+
+SELECT a.name FROM animals a JOIN owners o ON a.owner_id = o.id JOIN species s ON a.species_id = s.id WHERE s.name = 'Digimon' AND o.full_name = 'Jennifer Orwell';
+
+SELECT a.name FROM animals a JOIN owners o ON a.owner_id = o.id WHERE a.escape_attempts = 0 AND o.full_name = 'Dean Winchester';
+
+SELECT combined.full_name FROM (SELECT o.full_name, COUNT (a.name) AS animal_number FROM owners o LEFT JOIN animals a ON o.id = a.owner_id
+GROUP BY o.full_name) AS combined WHERE combined.animal_number = (SELECT MAX (animal_number)FROM (SELECT o.full_name,COUNT (a.name) AS
+animal_number FROM owners o LEFT JOIN animals a ON o.id = a.owner_id GROUP BY o.full_name) AS xx);
--- schema.sql
+++ schema.sql
@@ -12,3 +12,23 @@ CREATE TABLE animals (
 -- add a column to the table
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(100);
+
+-- project for multiple tables
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(50),
+    age INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(60),
+    PRIMARY KEY (id)
+);
+
+-- modifying table animals again
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT CONSTRAINT species_fk REFERENCES species (id);
+ALTER TABLE animals ADD COLUMN owner_id INT CONSTRAINT owner_fk REFERENCES owners (id);
