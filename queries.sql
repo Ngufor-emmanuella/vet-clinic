a554a84f29a3f8fa6568bdffcd7d95211eaae13f
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-01-07 03:18:35 +0100

    Fix synax errors

--- queries.sql
+++ queries.sql
@@ -51,18 +51,18 @@ SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
 
 -- project for multiple table relation
-SELECT a.name FROM animals a JOIN owners o ON a.owner_id = o.id WHERE o.full_name = 'Melody Pond';
+SELECT n.name FROM animals n JOIN owners w ON n.owner_id = w.id WHERE w.full_name = 'Melody Pond';
 
-SELECT a.name FROM animals a JOIN species s ON a.species_id = s.id WHERE s.name = 'Pokemon';
+SELECT n.name FROM animals n JOIN species s ON n.species_id = s.id WHERE s.name = 'Pokemon';
 
-SELECT o.full_name, a.name FROM owners o LEFT JOIN animals a ON o.id = a.owner_id;
+SELECT n.full_name, n.name FROM owners w LEFT JOIN animals a ON w.id = n.owner_id;
 
-SELECT count(*), s.name FROM animals a JOIN species s ON a.species_id = s.id GROUP BY s.name;
+SELECT count(*), s.name FROM animals n JOIN species s ON n.species_id = s.id GROUP BY s.name;
 
-SELECT a.name FROM animals a JOIN owners o ON a.owner_id = o.id JOIN species s ON a.species_id = s.id WHERE s.name = 'Digimon' AND o.full_name = 'Jennifer Orwell';
+SELECT n.name FROM animals n JOIN owners w ON n.owner_id = w.id JOIN species s ON n.species_id = s.id WHERE s.name = 'Digimon' AND w.full_name = 'Jennifer Orwell';
 
-SELECT a.name FROM animals a JOIN owners o ON a.owner_id = o.id WHERE a.escape_attempts = 0 AND o.full_name = 'Dean Winchester';
+SELECT n.name FROM animals n JOIN owners w ON n.owner_id = w.id WHERE n.escape_attempts = 0 AND w.full_name = 'Dean Winchester';
 
-SELECT combined.full_name FROM (SELECT o.full_name, COUNT (a.name) AS animal_number FROM owners o LEFT JOIN animals a ON o.id = a.owner_id
-GROUP BY o.full_name) AS combined WHERE combined.animal_number = (SELECT MAX (animal_number)FROM (SELECT o.full_name,COUNT (a.name) AS
-animal_number FROM owners o LEFT JOIN animals a ON o.id = a.owner_id GROUP BY o.full_name) AS xx);
+SELECT combined.full_name FROM (SELECT w.full_name, COUNT (n.name) AS animal_number FROM owners w LEFT JOIN animals n ON w.id = n.owner_id
+GROUP BY w.full_name) AS combined WHERE combined.animal_number = (SELECT MAX (animal_number)FROM (SELECT w.full_name,COUNT (n.name) AS
+animal_number FROM owners w LEFT JOIN animals n ON w.id = n.owner_id GROUP BY w.full_name) AS xx);
